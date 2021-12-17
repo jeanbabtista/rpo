59e9393b7b0b5f614c3395c152be0b09520a3b05
Jean <bedrac.zan@gmail.com>
2021-12-17 13:20:34 +0100

    deploying to heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const app = express()
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 const url = `http://localhost:${PORT}`
 
 app.get('/', (req, res) => res.json({ message: 'Hello World!' }))
