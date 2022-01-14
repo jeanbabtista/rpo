daefe3a24b8cb76f6e7f4c22aeae9a7ff4d0ec33
Jean Babtista <bedrac.zan@gmail.com>
2022-01-15 00:03:51 +0100

    added database, google auth; TODO - enable CORS

--- client/src/api/auth/index.js
+++ client/src/api/auth/index.js
@@ -1,5 +1,15 @@
+import axios from 'axios'
+
 const login = {
-  google: () => window.open('http://localhost:5000/auth/google', '_self'),
+  google: async (data) => {
+    const response = await axios.post(
+      'http://localhost:5000/auth/google',
+      data,
+      { withCredentials: true, headers: { 'Access-Control-Allow-Origin': '*' } }
+    )
+
+    return response.data
+  },
 }
 
 export default login
--- client/src/pages/Signin/index.js
+++ client/src/pages/Signin/index.js
@@ -1,18 +1,29 @@
 import React from 'react'
-
-// styles
-import Button from '@mui/material/Button'
+import GoogleLogin from 'react-google-login'
 
 // api
 import login from '../../api/auth'
 
 const Signin = () => {
+  const handleLogin = async (googleData) => {
+    try {
+      const response = await login.google(googleData)
+      console.log('Successfully posted google login:', response)
+    } catch (e) {
+      console.log('Error handling login:', e.message)
+    }
+  }
+
   return (
     <div>
       <h1>Login with Google</h1>
-      <Button variant="contained" onClick={() => login.google()}>
-        Login
-      </Button>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText="Log in with Google"
+        onSuccess={handleLogin}
+        onFailure={handleLogin}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   )
 }
